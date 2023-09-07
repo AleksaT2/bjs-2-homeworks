c746faeae306859e1aad17413c25c418b6244c1f
Alexa <sasha.tatanova@yandex.ru>
2023-09-07 23:21:55 +0300

    hw obj

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+   ;this.name = name;
+   this.gender = gender;
+   this.age = age
+   this.marks = []; 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('marks')) {
+    this.marks = [...this.marks, ...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty('marks') && this.marks.length != 0) {
+        let sum = 0;
+        for (mark of this.marks) {
+            sum += mark;
+        }
+        return sum / this.marks.length;
+      }
+      else {
+        return 0;
+      }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
